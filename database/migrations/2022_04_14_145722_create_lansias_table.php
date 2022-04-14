b9e49421563f0b3d2bd85c3c1079e84389f65ddb
b0rnt0ber00t <lanabackend@gmail.com>
2022-04-14 22:37:14 +0700

    update database, tambah user default

--- database/migrations/2022_04_14_145722_create_lansias_table.php
+++ database/migrations/2022_04_14_145722_create_lansias_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('lansias', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('jk');
+            $table->integer('usia');
+            $table->float('bb');
+            $table->string('tensi');
             $table->timestamps();
         });
     }
